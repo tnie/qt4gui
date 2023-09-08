d71e6733b0d729801a09f665bcfc9037161d836d
niel <nielong2014@gmail.com>
2023-09-08 10:51:57 +0800

    P[1-21] 中的例子

---
+++ hello/gotocelldialog.cpp
@@ -0,0 +1,28 @@
+﻿#include "gotocelldialog.h"
+#include "ui_gotocelldialog.h"
+#include <QRegExp>
+#include <QRegExpValidator>
+
+GoToCellDialog::GoToCellDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::GoToCellDialog)
+{
+    ui->setupUi(this);
+    const QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
+    ui->lineEdit->setValidator(new QRegExpValidator(regExp, this));
+    connect(ui->okButton, &QPushButton::clicked, this, &QDialog::accept);
+    connect(ui->cancelButton, &QPushButton::clicked, this, &QDialog::reject);
+    ui->lineEdit->setText("invalid");
+}
+
+GoToCellDialog::~GoToCellDialog()
+{
+    delete ui;
+}
+
+void GoToCellDialog::on_lineEdit_textChanged(const QString &arg1)
+{
+//    ui->okButton->setEnabled(!arg1.isEmpty());
+    ui->okButton->setEnabled(ui->lineEdit->hasAcceptableInput());
+}
+
---
+++ hello/main.cpp
@@ -0,0 +1,67 @@
+﻿#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QWidget>
+#include <QSpinBox>
+#include <QSlider>
+#include <QHBoxLayout>
+
+void hello()
+{
+//    QLabel * label = new QLabel("Hello Qt!");
+    QLabel * label = new QLabel("<h2><i>Hello </i>"
+                                "<font color=red>Qt!</font></h2>");
+    label->show();
+}
+
+void quit()
+{
+
+}
+
+void age()
+{
+    QWidget * window = new QWidget();
+    window->setWindowTitle("Enter Your Age");
+
+    QSpinBox * spinBox = new QSpinBox();
+    spinBox->setRange(0, 130);
+    QSlider * slider = new QSlider(Qt::Horizontal);
+    slider->setRange(0, 130);
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+                     slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)),
+                     spinBox, SLOT(setValue(int)));
+    spinBox->setValue(35);
+
+    QHBoxLayout * layout = new QHBoxLayout();
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+
+    window->setLayout(layout);
+    window->show();
+}
+
+#include "ui_gotocelldialog.h"
+#include "gotocelldialog.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    if(false)
+    {
+        QPushButton *btn = new QPushButton("Quit");
+        QObject::connect(btn, SIGNAL(clicked()), &a, SLOT(quit()));
+        btn->show();
+    }
+    if(false)
+    {
+        Ui::GoToCellDialog ui;
+        QDialog *dialog = new QDialog;
+        ui.setupUi(dialog);
+        dialog->show();
+    }
+    GoToCellDialog *dialog = new GoToCellDialog;
+    dialog->show();
+
+    return a.exec();
+}
